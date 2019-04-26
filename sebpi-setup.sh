10debfc09f052e9d9cb3b59c65474fbfbd9f8231
Sébastien Kalb <skalb@altissia.com>
2019-04-26 20:50:05 +0200

    Plain cleanups

--- sebpi-setup.sh
+++ sebpi-setup.sh
@@ -22,23 +22,12 @@ SEBPI_UPDATE_SCRIPT=/boot/sebpi-update.sh
 SEBPI_INSTALLED_FILE=/boot/sebpi.installed
 SEBPI_LOCALE_GEN_FILE=/boot/sebpi.locale.gen
 
-SEBPI_INSTALLED=0
-SEBPI_LOCALE_GEN=0
+if [ ! -e "$SEBPI_LOCALE_GEN_FILE" ]; then
+	# Fix locale issues on Debian
+	sudo sed -i 's/^AcceptEnv.*/#AcceptEnv LANG LC_*/' /etc/ssh/sshd_config
 
-if [ -e "$SEBPI_INSTALLED_FILE" ]; then
-	echo "This is not the first time this script is run, ignoring some one-time settings."
-	SEBPI_INSTALLED=1
-fi
-
-if [ -e "$SEBPI_LOCALE_GEN_FILE" ]; then
-	SEBPI_LOCALE_GEN=1
-fi
-
-# Fix locale issues on Debian
-sudo sed -i 's/^AcceptEnv.*/#AcceptEnv LANG LC_*/' /etc/ssh/sshd_config
-
-if [ "$SEBPI_LOCALE_GEN" == 0 ]; then
-	SEBPI_LOCALE="en_GB.UTF-8"
+	local SEBPI_LOCALE="en_GB.UTF-8"
+	export LANGUAGE=$SEBPI_LOCALE
 	sudo locale-gen "$SEBPI_LOCALE"
 	sudo update-locale LC_ALL=$SEBPI_LOCALE LANG=$SEBPI_LOCALE
 
@@ -46,7 +35,7 @@ if [ "$SEBPI_LOCALE_GEN" == 0 ]; then
 fi
 
 sudo mkdir -p /etc/pihole
-sudo rm /etc/pihole/setupVars.conf
+sudo rm -f /etc/pihole/setupVars.conf
 sudo touch /etc/pihole/setupVars.conf
 sudo sh -c 'echo "PIHOLE_INTERFACE=eth0" >> /etc/pihole/setupVars.conf'
 sudo sh -c 'echo "IPV4_ADDRESS=192.168.1.2/24" >> /etc/pihole/setupVars.conf'
@@ -67,14 +56,14 @@ sudo sh -c './install_pihole.sh --unattended'
 rm install_pihole.sh
 echo | pihole -a -p
 
-if [ ! -z "$PUSHBULLET_API_KEY" ]; then
+if [ -n "$PUSHBULLET_API_KEY" ]; then
 	if ! grep -q "$PUSHBULLET_API_KEY" "/etc/rc.local"; then
 		# send a pushbullet notification on login
 		sudo sed -i '$i'"$(echo "curl -s -u $PUSHBULLET_API_KEY: https://api.pushbullet.com/v2/pushes -d type=note -d title=Raspberry\\\ Pi -d body=Raspberry\\\ Pi\\\ is\\\ up! > /dev/null")" /etc/rc.local
 	fi
 fi
 
-if [ "$SEBPI_INSTALLED" == 0 ]; then
+if [ ! -e "$SEBPI_INSTALLED_FILE" ]; then
 	# https://www.zdnet.com/article/raspberry-pi-extending-the-life-of-the-sd-card/
 	# sudo sh -c 'echo "tmpfs /tmp tmpfs defaults,noatime,nosuid,size=100m 0 0" >> /etc/fstab'
 	# sudo sh -c 'echo "tmpfs /var/tmp tmpfs defaults,noatime,nosuid,size=30m 0 0" >> /etc/fstab'
@@ -85,7 +74,7 @@ if [ "$SEBPI_INSTALLED" == 0 ]; then
 	sudo sed -i '$i'"$(echo "sudo sh -c 'echo 0 > /sys/class/leds/led0/brightness'")" /etc/rc.local
 	sudo sed -i '$i'"$(echo "sudo sh -c 'echo 0 > /sys/class/leds/led1/brightness'")" /etc/rc.local
 
-	sudo $SEBPI_UPDATE_SCRIPT
+	sudo sh -c "$SEBPI_UPDATE_SCRIPT"
 
 	# install speedtest-cli
 	wget -O /home/pi/speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
